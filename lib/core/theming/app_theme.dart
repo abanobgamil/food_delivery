2efee403ee4674323d54193b39672e9cde382b7d
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-01-30 11:09:00 +0200

    - create app_theme

---
+++ lib/core/theming/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/core/theming/app_colors.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    fontFamily: 'Montserrat',
+    scaffoldBackgroundColor: AppColors.white,
+    useMaterial3: false,
+  );
+}
