464203e1dcfb2debeca61544b93731c82023ca31
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-01-30 11:08:36 +0200

    - create app_routes

---
+++ lib/core/routing/app_route.dart
@@ -0,0 +1,8 @@
+import 'package:food_delivery/core/routing/app_route_names.dart';
+import 'package:food_delivery/features/login/presentaion/screens/login_screen.dart';
+
+class AppRoutes{
+  static final routes = {
+    AppRoutesNames.login: (context) => LoginScreen(),
+  };
+}
