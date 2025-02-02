68760d4f8f63ed559119f3951c965eb5bbe19a83
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-02 23:56:53 +0200

    - add new colors

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -4,6 +4,10 @@ class AppColors {
   static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF000000);
   static const blue = Color(0xFF131222);
-
+  static const blue2 = Color(0xFFF2F5F8);
+  static const blue3 = Color(0xFFCFD9E6);
+  static const gray4 = Color(0xFF979797);
+  static const gray5 = Color(0xFF808080);
+  static const orange = Color(0xFFff6801);
 
 }
