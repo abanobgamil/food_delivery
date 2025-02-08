459384a9dd63b2e2f07475504c494cc1d71eb953
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-08 03:09:42 +0200

    - initialize Firebase - wrap MyApp with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_delivery/my_app.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
-      builder: (context) => const MyApp(),
+      builder: (context) => ProviderScope(child: const MyApp()),
     ),
   );
 }
