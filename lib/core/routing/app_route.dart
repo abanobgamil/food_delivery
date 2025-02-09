1d8d4483a80ce43fb7d52d8b19fa0baf96ca51a4
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-09 21:50:42 +0200

    - create app layout screen - add app layout route - add app layout route name

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -1,8 +1,10 @@
 import 'package:food_delivery/core/routing/app_route_names.dart';
+import 'package:food_delivery/features/app_layout/app_layout_screen.dart';
 import 'package:food_delivery/features/login/presentaion/screens/login_screen.dart';
 
 class AppRoutes{
   static final routes = {
     AppRoutesNames.login: (context) => LoginScreen(),
+    AppRoutesNames.appLayout: (context) => AppLayoutScreen(),
   };
 }
--- lib/core/routing/app_route_names.dart
+++ lib/core/routing/app_route_names.dart
@@ -1,3 +1,4 @@
 class AppRoutesNames{
   static const String login = "/login";
+  static const String appLayout = "/appLayout";
 }
