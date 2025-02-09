18e6cd4255245858353c46a0172658ced3e65087
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-09 21:56:35 +0200

    - enforce portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_delivery/my_app.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   await Firebase.initializeApp();
   runApp(
     DevicePreview(
