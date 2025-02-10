2dbc9fc72edcec01bb880078832a88190a563fe1
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-10 20:02:50 +0200

    - create app_layout_screen - add HomeScreen widget as a body

--- lib/features/app_layout/app_layout_screen.dart
+++ lib/features/app_layout/app_layout_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/features/home/presentation/screen/home_screen.dart';
 
 class AppLayoutScreen extends StatelessWidget {
   const AppLayoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-
+    return Scaffold(
+      appBar: AppBar(),
+      body: HomeScreen(),
     );
   }
 }
