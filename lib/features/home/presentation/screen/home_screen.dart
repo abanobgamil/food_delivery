57a93ddca9273c5c26afd38e021ba2929de25975
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-13 01:15:32 +0200

    - add CustomCircularProgressIndicator widget - add RestaurantItem widget to home screen list view - replace static RestaurantItem to dynamic RestaurantItem in home screen list view

--- lib/features/home/presentation/screen/home_screen.dart
+++ lib/features/home/presentation/screen/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:food_delivery/core/widgets/custom_text_form_field.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/core/widgets/custom_circular_progress_indicator.dart';
 import 'package:food_delivery/features/home/presentation/providers/home_provider.dart';
+import 'package:food_delivery/features/home/presentation/providers/home_states.dart';
 import 'package:food_delivery/features/home/presentation/widgets/restaurant_item.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,15 +11,39 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Consumer(builder: (BuildContext context, WidgetRef ref, Widget? child) {
-        //   return CustomTextFormField(textController: ref.read(homeNotifierProvider.notifier).searchController,
-        //     hint: "search", type: TextInputType.text,prefix: Icons.search,);
-        // },
-        // ),
-        RestaurantItem()
-      ],
-    );
-  }
-}
+    return Consumer(
+        builder: (BuildContext context, WidgetRef ref, Widget? child){
+      final homeState = ref.watch(homeNotifierProvider);
+      final provider = ref.read(homeNotifierProvider.notifier);
+
+      if (homeState is HomeLoadingRestaurantState) {
+        return Center(child: CustomCircularProgressIndicator());
+      }
+      else if (homeState is HomeSuccessRestaurantState) {
+        return Column(
+          children: [
+            SizedBox(
+              height: 240.h,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.all(16.r),
+                itemBuilder: (BuildContext context, int index) {
+                 return RestaurantItem(
+                    title: provider.restaurantList[index].restaurantName ,
+                    imageUrl: provider.restaurantList[index].restaurantImage,
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                 return SizedBox(width: 10.w,);
+                },
+                itemCount: provider.restaurantList.length,
+              ),
+            )
+          ],
+        );
+      }else{
+        return const Center(child: Text('Error fetching data'));
+      }
+        }
+  );
+}}
