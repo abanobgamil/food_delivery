56f17371b9125d603ca596e0be965f017cc304f3
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-12 23:38:31 +0200

    - implement HomeProvider class to handle restaurant data - create HomeRepo interface - create HomeRepoImpl implement to get restaurant data

--- lib/features/home/presentation/providers/home_provider.dart
+++ lib/features/home/presentation/providers/home_provider.dart
@@ -1,18 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_delivery/features/home/data/model/restaurant_model.dart';
+import 'package:food_delivery/features/home/data/repos/home_repo_impl.dart';
+import 'package:food_delivery/features/home/domain/repos/home_repo.dart';
 import 'package:food_delivery/features/home/presentation/providers/home_states.dart';
 
-class HomeProvider extends StateNotifier<HomeStates>{
-  HomeProvider(this.ref) : super(InitialHomeState());
+class HomeProvider extends StateNotifier<HomeStates> {
+  HomeProvider(this.ref, this._homeRepo) : super(InitialHomeState());
 
   final Ref ref;
   final searchController = TextEditingController();
+  final HomeRepo _homeRepo;
+  List<RestaurantModel> restaurantList = [];
 
+  Future<void> getRestaurant() async{
+    state = HomeLoadingRestaurantState();
+    try {
+      restaurantList =  await _homeRepo.getRestaurant();
+      state = HomeSuccessRestaurantState();
+    } on FirebaseException catch (e) {
+      state = HomeErrorRestaurantState(message: e.message ?? 'An error occurred');
+    }
 
+  }
 }
 
-
-
-final homeNotifierProvider = StateNotifierProvider<HomeProvider, HomeStates>((ref) {
-  return HomeProvider(ref);
+final homeNotifierProvider =
+    StateNotifierProvider<HomeProvider, HomeStates>((ref) {
+  return HomeProvider(ref, HomeRepoImpl());
 });
