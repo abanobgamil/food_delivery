33477c50a6a7ff72543115a576f51c80df9e79b0
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-12 23:37:21 +0200

    - create HomeStates sealed class - create InitialHomeState - create HomeLoadingRestaurantState - create HomeSuccessRestaurantState - create HomeErrorRestaurantState

--- lib/features/home/presentation/providers/home_states.dart
+++ lib/features/home/presentation/providers/home_states.dart
@@ -1,3 +1,16 @@
 sealed class HomeStates{}
 
 class InitialHomeState extends HomeStates{}
+
+class HomeLoadingRestaurantState extends HomeStates{}
+
+class HomeSuccessRestaurantState extends HomeStates{}
+
+class HomeErrorRestaurantState extends HomeStates {
+  final String message;
+
+  HomeErrorRestaurantState({
+    required this.message,
+  });
+}
+
