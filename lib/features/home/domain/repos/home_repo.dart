e79f9c8313396bd4cf62564de4bd8365c09f5165
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-13 23:37:08 +0200

    - add getOffers in home_repo

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,7 +1,10 @@
+import 'package:food_delivery/features/home/data/model/offers_model.dart';
 import 'package:food_delivery/features/home/data/model/restaurant_model.dart';
 
 abstract class HomeRepo {
 
   Future<List<RestaurantModel>> getRestaurant();
+
+  Future<List<OffersModel>> getOffers();
 
 }
