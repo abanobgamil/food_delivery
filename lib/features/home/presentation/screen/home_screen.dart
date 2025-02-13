fcfa27983f648058a519eff783beafc4a06053e8
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-13 23:40:16 +0200

    - add orange color to CustomCircularProgressIndicator

--- lib/features/home/presentation/screen/home_screen.dart
+++ lib/features/home/presentation/screen/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/core/theming/app_colors.dart';
 import 'package:food_delivery/core/widgets/custom_circular_progress_indicator.dart';
 import 'package:food_delivery/core/widgets/custom_text_w700.dart';
-import 'package:food_delivery/features/home/presentation/providers/home_provider.dart';
-import 'package:food_delivery/features/home/presentation/providers/home_states.dart';
+import 'package:food_delivery/features/home/presentation/providers/restaurant_section_home_provider/restaurant_section_home_provider.dart';
+import 'package:food_delivery/features/home/presentation/providers/restaurant_section_home_provider/restaurant_section_home_states.dart';
 import 'package:food_delivery/features/home/presentation/widgets/restaurant_item.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,7 +19,7 @@ class HomeScreen extends StatelessWidget {
       final provider = ref.read(homeNotifierProvider.notifier);
 
       if (homeState is HomeLoadingRestaurantState) {
-        return Center(child: CustomCircularProgressIndicator());
+        return Center(child: CustomCircularProgressIndicator(color: AppColors.orange,width: 20.w,height: 20.w,));
       }
       else if (homeState is HomeSuccessRestaurantState) {
         return Padding(
