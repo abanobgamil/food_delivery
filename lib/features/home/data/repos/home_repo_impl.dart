84913d30aad57aa0c33749c79be684f73ced4298
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-13 23:38:24 +0200

    - implement get offers data

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:food_delivery/core/networking/firebase_instances.dart';
+import 'package:food_delivery/features/home/data/model/offers_model.dart';
 import 'package:food_delivery/features/home/data/model/restaurant_model.dart';
 import 'package:food_delivery/features/home/domain/repos/home_repo.dart';
 
@@ -15,6 +16,22 @@ class HomeRepoImpl extends HomeRepo{
       }
 
       return restaurantList;
+    } catch (e, stackTrace) {
+      print('Error fetching data: $e'+"$stackTrace");
+      return [];
+    }
+  }
+
+  @override
+  Future<List<OffersModel>> getOffers() async{
+    List<OffersModel> offersList = [];
+    try {
+      QuerySnapshot offersSnapshot = await FirebaseInstances.fireStore.collection('offers').get();
+      for (var offersDoc in offersSnapshot.docs) {
+        offersList.add(OffersModel.fromJson(offersDoc.data() as Map<String, dynamic>));
+      }
+
+      return offersList;
     } catch (e, stackTrace) {
       print('Error fetching data: $e'+"$stackTrace");
       return [];
