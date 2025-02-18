4e656d38c7ebb5cc87ad6b4c7b4e55b954955245
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-18 23:53:59 +0200

    - add restaurant screen content - add rest name W700 - add rating W400 - add image to restaurant screen

--- lib/features/restaurant_page/presentation/screens/restaurant_screen.dart
+++ lib/features/restaurant_page/presentation/screens/restaurant_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_delivery/core/widgets/custom_text_w400.dart';
+import 'package:food_delivery/core/widgets/custom_text_w700.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [
-
-    ],);
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Image.network("",height: 100.h),
+            Padding(
+              padding: EdgeInsets.only(top: 50.h),
+              child: Column(
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        CustomTextW700(text: "rest name"),
+                        Row(
+                          children: [
+                            Icon(Icons.star),
+                            CustomTextW400(text: "4.0")
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
