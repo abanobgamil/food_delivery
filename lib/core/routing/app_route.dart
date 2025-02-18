a2836164ab50b48ff960e3a81a54d74b8d6a0196
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-18 23:12:11 +0200

    - add restaurantDetails route - add restaurant screen

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -1,10 +1,12 @@
 import 'package:food_delivery/core/routing/app_route_names.dart';
 import 'package:food_delivery/features/app_layout/app_layout_screen.dart';
 import 'package:food_delivery/features/login/presentaion/screens/login_screen.dart';
+import 'package:food_delivery/features/restaurant_page/presentation/screens/restaurant_screen.dart';
 
 class AppRoutes{
   static final routes = {
     AppRoutesNames.login: (context) => LoginScreen(),
     AppRoutesNames.appLayout: (context) => AppLayoutScreen(),
+    AppRoutesNames.restaurantDetails: (context) => RestaurantScreen(),
   };
 }
