5b0a422c9f11fe154917cc0db8843ed32d73cf7e
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-21 17:54:49 +0200

    - set up dependency injection using get_it

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_delivery/core/dependency_injection/setup_git.dart';
 import 'package:food_delivery/my_app.dart';
 
 void main() async{
+  setupLocator();
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
