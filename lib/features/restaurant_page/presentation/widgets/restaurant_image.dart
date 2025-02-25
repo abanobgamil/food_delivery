275ac0dfe1af4d07c45ccf35ab380cee5fe2cb52
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-25 17:09:29 +0200

    - replace Image.network with CachedNetworkImage - add Shimmer loading animation

--- lib/features/restaurant_page/presentation/widgets/restaurant_image.dart
+++ lib/features/restaurant_page/presentation/widgets/restaurant_image.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class RestaurantImage extends StatelessWidget {
   const RestaurantImage({super.key, required this.image});
@@ -7,8 +9,18 @@ class RestaurantImage extends StatelessWidget {
   final String image;
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      image,
+   return CachedNetworkImage(
+      imageUrl: image,
+     placeholder: (context, url) => Shimmer.fromColors(
+       baseColor: Colors.grey[300]!,
+       highlightColor: Colors.grey[100]!,
+       child: Container(
+         height: 200.h,
+         width: double.infinity,
+         color: Colors.white,
+       ),
+     ),
+      errorWidget: (context, url, error) => Icon(Icons.error),
       height: 200.h,
       width: double.infinity,
       fit: BoxFit.fill,
