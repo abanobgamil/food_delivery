d73118ac7d816471c1be183bab7facd86479ac3a
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-25 22:03:45 +0200

    - add ProductsModel in ProductDetailsScreen to receive data from restaurant screen

--- lib/features/product_details/presentation/product_details_screen.dart
+++ lib/features/product_details/presentation/product_details_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/features/restaurant_page/data/models/products_model.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
-  const ProductDetailsScreen({super.key});
+  const ProductDetailsScreen({super.key, required this.productsModel});
+
+  final ProductsModel productsModel ;
 
   @override
   Widget build(BuildContext context) {
