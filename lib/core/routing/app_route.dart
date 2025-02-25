895dd7488e3f621042f9dd8027ea487df64c8936
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-25 22:04:14 +0200

    - add product details route - add product details screen

--- lib/core/routing/app_route.dart
+++ lib/core/routing/app_route.dart
@@ -3,6 +3,8 @@ import 'package:food_delivery/core/routing/app_route_names.dart';
 import 'package:food_delivery/features/app_layout/app_layout_screen.dart';
 import 'package:food_delivery/features/home/data/model/restaurant_model.dart';
 import 'package:food_delivery/features/login/presentaion/screens/login_screen.dart';
+import 'package:food_delivery/features/product_details/presentation/product_details_screen.dart';
+import 'package:food_delivery/features/restaurant_page/data/models/products_model.dart';
 import 'package:food_delivery/features/restaurant_page/presentation/screens/restaurant_screen.dart';
 
 class AppRoutes{
@@ -12,6 +14,10 @@ class AppRoutes{
     AppRoutesNames.restaurantDetails: (context) {
       final args = ModalRoute.of(context)!.settings.arguments as RestaurantModel;
      return RestaurantScreen(restaurantData: args);
+    },
+    AppRoutesNames.productDetails: (context) {
+      final args = ModalRoute.of(context)!.settings.arguments as ProductsModel;
+     return ProductDetailsScreen(productsModel: args);
     }
   };
 }
