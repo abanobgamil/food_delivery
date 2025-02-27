c5240f76762e23fbcbc57366e2af9549392b4aa7
Abanoub Gamil <abanoubgamil189@gmail.com>
2025-02-27 23:51:49 +0200

    - create ProductDetailsRepoImpl

---
+++ lib/features/product_details/data/repo/repo_impl.dart
@@ -0,0 +1,10 @@
+import 'package:food_delivery/features/product_details/domain/repos/product_details_repo.dart';
+
+class ProductDetailsRepoImpl extends ProductDetailsRepo{
+  @override
+  Future<void> addToCart() {
+    // TODO: implement addToCart
+    throw UnimplementedError();
+  }
+
+}
